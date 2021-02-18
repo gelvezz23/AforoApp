7ef3522d51712497bfa6e51b725348b283d5613d
CarlosG <gelvezz223@gmail.com>
2021-02-18 12:06:57 -0500

    add count in render

--- src/App.js
+++ src/App.js
@@ -1,106 +1,129 @@
-import React from "react";
-import ReactDOM from "react-dom";
+/** @format */
 
-import * as cocoSsd from "@tensorflow-models/coco-ssd";
-import "@tensorflow/tfjs";
-import "./styles.css";
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import * as cocoSsd from '@tensorflow-models/coco-ssd';
+import Person from './components/person';
+import '@tensorflow/tfjs';
+import './styles.css';
 
 export default class App extends React.Component {
-  videoRef = React.createRef();
-  canvasRef = React.createRef();
+	constructor() {
+		super();
+		this.state = {
+			count: 0,
+		};
+	}
 
-  componentDidMount() {
-    if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
-      const webCamPromise = navigator.mediaDevices
-        .getUserMedia({
-          audio: false,
-          video: {
-            facingMode: "user"
-          }
-        })
-        .then(stream => {
-          window.stream = stream;
-          this.videoRef.current.srcObject = stream;
-          return new Promise((resolve, reject) => {
-            this.videoRef.current.onloadedmetadata = () => {
-              resolve();
-            };
-          });
-        });
-      const modelPromise = cocoSsd.load();
-      Promise.all([modelPromise, webCamPromise])
-        .then(values => {
-          this.detectFrame(this.videoRef.current, values[0]);
-        })
-        .catch(error => {
-          console.error(error);
-        });
-    }
-  }
+	videoRef = React.createRef();
+	canvasRef = React.createRef();
 
-  detectFrame = (video, model) => {
-    model.detect(video).then(predictions => {
-      this.renderPredictions(predictions);
-      requestAnimationFrame(() => {
-        this.detectFrame(video, model);
-      });
-    });
-  };
+	componentDidMount() {
+		if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+			const webCamPromise = navigator.mediaDevices
+				.getUserMedia({
+					audio: false,
+					video: {
+						facingMode: 'user',
+					},
+				})
+				.then((stream) => {
+					window.stream = stream;
+					this.videoRef.current.srcObject = stream;
+					return new Promise((resolve, reject) => {
+						this.videoRef.current.onloadedmetadata = () => {
+							resolve();
+						};
+					});
+				});
+			const modelPromise = cocoSsd.load();
+			Promise.all([modelPromise, webCamPromise])
+				.then((values) => {
+					this.detectFrame(this.videoRef.current, values[0]);
+				})
+				.catch((error) => {
+					console.error(error);
+				});
+		}
+	}
 
-  renderPredictions = predictions => {
-    const ctx = this.canvasRef.current.getContext("2d");
-    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
-    // Font options.
-    const font = "16px sans-serif";
-    ctx.font = font;
-    ctx.textBaseline = "top";
-    predictions.forEach(prediction => {
-      const x = prediction.bbox[0];
-      const y = prediction.bbox[1];
-      const width = prediction.bbox[2];
-      const height = prediction.bbox[3];
-      // Draw the bounding box.
-      ctx.strokeStyle = "#00FFFF";
-      ctx.lineWidth = 4;
-      ctx.strokeRect(x, y, width, height);
-      // Draw the label background.
-      ctx.fillStyle = "#00FFFF";
-      const textWidth = ctx.measureText(prediction.class).width;
-      const textHeight = parseInt(font, 10); // base 10
-      ctx.fillRect(x, y, textWidth + 4, textHeight + 4);
-    });
+	detectFrame = (video, model) => {
+		model.detect(video).then((predictions) => {
+			this.renderPredictions(predictions);
+			requestAnimationFrame(() => {
+				this.detectFrame(video, model);
+			});
+		});
+	};
 
-    predictions.forEach(prediction => {
-      const x = prediction.bbox[0];
-      const y = prediction.bbox[1];
-      // Draw the text last to ensure it's on top.
-      ctx.fillStyle = "#000000";
-      ctx.fillText(prediction.class, x, y);
-    });
-  };
+	renderPredictions = (predictions) => {
+		const ctx = this.canvasRef.current.getContext('2d');
+		ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+		// Font options.
+		const font = '16px sans-serif';
+		ctx.font = font;
+		ctx.textBaseline = 'top';
+		let NumbersPerson = 0;
+		this.setState({ count: 0 });
 
-  render() {
-    return (
-      <div>
-        <video
-          className="size"
-          autoPlay
-          playsInline
-          muted
-          ref={this.videoRef}
-          width="600"
-          height="500"
-        />
-        <canvas
-          className="size"
-          ref={this.canvasRef}
-          width="600"
-          height="500"
-        />
-      </div>
-    );
-  }
+		predictions.forEach((prediction) => {
+			const x = prediction.bbox[0];
+			const y = prediction.bbox[1];
+			const width = prediction.bbox[2];
+			const height = prediction.bbox[3];
+			// Draw the bounding box.
+			ctx.strokeStyle = '#00FFFF';
+			ctx.lineWidth = 4;
+			ctx.strokeRect(x, y, width, height);
+			// Draw the label background.
+			ctx.fillStyle = '#00FFFF';
+			const textWidth = ctx.measureText(prediction.class).width;
+			const textHeight = parseInt(font, 10); // base 10
+			ctx.fillRect(x, y, textWidth + 4, textHeight + 4);
+
+			/* Codigo de Carlos y andres */
+			const person = 'person';
+			if (prediction.class == person) {
+				NumbersPerson++;
+			}
+			this.setState({ count: NumbersPerson });
+			console.log(this.state.count);
+		});
+
+		predictions.forEach((prediction) => {
+			const x = prediction.bbox[0];
+			const y = prediction.bbox[1];
+			// Draw the text last to ensure it's on top.
+			ctx.fillStyle = '#000000';
+			ctx.fillText(prediction.class, x, y);
+		});
+	};
+
+	render() {
+		return (
+			<>
+				<div className='App'>
+					<header className='App-header'>
+						<Person person={this.state.count} />
+						<div>
+							<video
+								className='size'
+								autoPlay
+								playsInline
+								muted
+								ref={this.videoRef}
+								width='600'
+								height='500'
+							/>
+							<canvas className='size' ref={this.canvasRef} width='600' height='500' />
+						</div>
+					</header>
+				</div>
+			</>
+		);
+	}
 }
 
-const rootElement = document.getElementById("root");
+const rootElement = document.getElementById('root');
 ReactDOM.render(<App />, rootElement);
