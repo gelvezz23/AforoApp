bc5a1c5442021e4d61b80e754be201ead5994d94
CarlosG <gelvezz223@gmail.com>
2021-02-18 12:17:18 -0500

    deploy

---
+++ build/precache-manifest.3e2ce359e9c32f36c1f562933bffb65c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "462f4a9da3561f52ecf3c87b36841a8e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b3145827636e8dc420b2",
+    "url": "/static/css/main.cee13bfe.chunk.css"
+  },
+  {
+    "revision": "1525fd42900628e4ed80",
+    "url": "/static/js/2.ead4ac1d.chunk.js"
+  },
+  {
+    "revision": "b3145827636e8dc420b2",
+    "url": "/static/js/main.9e1ca09d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
